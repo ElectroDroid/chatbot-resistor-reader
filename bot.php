b2786a6301e052d61a698c0b69e5054e59db6e1a
Yelly K <an.an.yelly@gmail.com>
2017-11-23 22:15:35 +0700

    Add 'Say Hello'

--- bot.php
+++ bot.php
@@ -11,16 +11,34 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
 			// Build message to reply back
+			
+			if(strpos($text,'Hello')!==false) {
+
 			$messages = [
+				'type' => 'text',
+				'text' => 'Hello! Let\'s me help you read resistance color code.'
+			];
+
+			}
+
+			else {
+				$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
+			}
+
+
+
+
+
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -45,3 +63,4 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+;;

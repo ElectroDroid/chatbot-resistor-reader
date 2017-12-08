736cc87097779f56a81087d5e9b428da69a743de
dargais <dargais_zzz@hotmail.com>
2017-12-08 19:45:42 +0700

    test

--- src/test.py
+++ src/test.py
@@ -4,8 +4,8 @@ import sys
 
 sitepackage = "D:\home\site\wwwroot\libs"
 
-#sitepackage = "..\libs"
-sys.path.append(sitepackage)
+sitepackage = "..\libs"
+#sys.path.append(sitepackage)
 
 #result = sys.argv[1]
 cv_version = cv2.__version__

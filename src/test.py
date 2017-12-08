4e166970c5b89dcd5078d9ae23c247ab3ca48c2a
Yelly K <an.an.yelly@gmail.com>
2017-12-08 18:34:22 +0700

    debug

--- src/test.py
+++ src/test.py
@@ -3,6 +3,11 @@
 import sys
 import cv2
 
+#sitepackage = "D:\home\site\wwwroot\libs"
+
+sitepackage = "../libs"
+sys.path.append(sitepackage)
+
 result = sys.argv[1]
 cv_version = cv2.__version__
 
